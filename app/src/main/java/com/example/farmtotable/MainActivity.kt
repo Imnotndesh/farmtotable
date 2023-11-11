018599d162deb96160268eacaaa6e17a18a1b082
Imnotndesh <slateinbox@gmail.com>
2023-11-11 14:24:50 +0300

    Done with cart fragment

--- app/src/main/java/com/example/farmtotable/MainActivity.kt
+++ app/src/main/java/com/example/farmtotable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.farmtotable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,19 +12,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val homeAppButton = findViewById<ImageView>(R.id.homeIcon)
         homeAppButton.setOnClickListener{
-                val setFragment = fragmentManager.beginTransaction()
-                setFragment.setReorderingAllowed(true)
-                setFragment.replace(R.id.fragmentContainerView, Homepage_fragment())
-                setFragment.addToBackStack(null)
-                setFragment.commit()
+            val setFragment = fragmentManager.beginTransaction()
+            setFragment.setReorderingAllowed(true)
+            setFragment.replace(R.id.fragmentContainerView, Homepage_fragment())
+            setFragment.addToBackStack(null)
+            setFragment.commit()
         }
         val cartAppButton = findViewById<ImageView>(R.id.cartIcon)
         cartAppButton.setOnClickListener{
             val setFragment = fragmentManager.beginTransaction()
-                setFragment.setReorderingAllowed(true)
-                setFragment.replace(R.id.fragmentContainerView, cart_fragment())
-                setFragment.addToBackStack(null)
-                setFragment.commit()
+            setFragment.setReorderingAllowed(true)
+            setFragment.replace(R.id.fragmentContainerView, cart_fragment())
+            setFragment.addToBackStack(null)
+            setFragment.commit()
+        }
+        val accountAppButton = findViewById<ImageView>(R.id.profIcon)
+        accountAppButton.setOnClickListener{
+            val setFragment = fragmentManager.beginTransaction()
+            setFragment.setReorderingAllowed(true)
+            setFragment.replace(R.id.fragmentContainerView, account_fragment())
+            setFragment.addToBackStack(null)
+            setFragment.commit()
         }
     }
 }
--- app/src/main/java/com/example/farmtotable/account_fragment.kt
+++ app/src/main/java/com/example/farmtotable/account_fragment.kt
@@ -5,10 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [cart_fragment.newInstance] factory method to
+ * Use the [account_fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class cart_fragment : Fragment() {
+class account_fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -38,10 +34,8 @@ class cart_fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.cart_fragment, container, false)
+        return inflater.inflate(R.layout.account_fragment, container, false)
     }
-
-
 
     companion object {
         /**
@@ -50,12 +44,12 @@ class cart_fragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment cart_fragment.
+         * @return A new instance of fragment account_fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            cart_fragment().apply {
+            account_fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
