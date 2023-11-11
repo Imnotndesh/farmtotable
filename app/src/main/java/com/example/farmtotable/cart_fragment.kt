f9fc7199d0235799ebe3cb2704e1e1e919b2e951
Imnotndesh <slateinbox@gmail.com>
2023-11-11 14:27:43 +0300

    Done with cart fragment

--- app/src/main/java/com/example/farmtotable/cart_fragment.kt
+++ app/src/main/java/com/example/farmtotable/cart_fragment.kt
@@ -40,26 +40,4 @@ class cart_fragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.cart_fragment, container, false)
     }
-
-
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment cart_fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            cart_fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
